4e5f58e253003ac0ab8a035fdd4c3cc330286959
bianlz <bianlanzhou@gmail.com>
2014-05-27 19:45:43 +0800

    new

---
+++ ConcurrencyCook/src/Test_4/test_9/ReportGenerator.java
@@ -0,0 +1,13 @@
+package Test_4.test_9;
+
+import java.util.concurrent.Callable;
+
+public class ReportGenerator implements Callable<String> {
+
+	@Override
+	public String call() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
